2d25a80529a5260e900dd925481bfbabdaf548be
TheAssassin <theassassin@users.noreply.github.com>
2018-03-11 13:43:53 +0100

    [debug] Upload AppImage to transfer.sh

--- travis/build-appimages.sh
+++ travis/build-appimages.sh
@@ -6,6 +6,11 @@ cd /build/out
     -u "gh-releases-zsync|AppImage|AppImageKit|continuous|appimagetool-$ARCH.AppImage.zsync" \
     appimagetool-"$ARCH".AppImage
 
+
+# DEBUG CODE
+curl --upload-file ./appimagetool-"$ARCH".AppImage https://transfer.sh
+
+
 ./appimagetool-"$ARCH".AppImage ./appimaged.AppDir/ -s -v \
     -u "gh-releases-zsync|AppImage|AppImageKit|continuous|appimaged-$ARCH.AppImage.zsync" \
     appimaged-"$ARCH".AppImage
